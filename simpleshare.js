b0bc1d7dfaa00c2a877d9202db4ae7bd776778d1
Balazs Dianiska <balazs@longlake.co.uk>
2011-02-22 13:02:28 +0000

    Support for custom services

--- simpleshare.js
+++ simpleshare.js
@@ -1,3 +1,5 @@
+
+
 Drupal.behaviors.simpleshare = function() {
   $('a.simpleshare:not(.processed)').each(function() {
     $(this).addClass('processed').click(function() {
@@ -7,8 +9,9 @@ Drupal.behaviors.simpleshare = function() {
       // Make an AJAX call for this link.
       var title = $(this).attr('title') ? $(this).attr('title') : $(document).attr('title');
       var url = $(this).attr('href');
+      var services = $(this).attr('rel').split(" ");
       var ajax_url = Drupal.settings.basePath + 'simpleshare';
-      $.get(ajax_url, {'title': title, 'url': url}, function(data) {
+      $.get(ajax_url, {'title': title, 'url': url, 'services': services}, function(data) {
         $(document.body).append(data);
         $('div.simpleshare-popup').show()
         .children('span.close').click(function() {
